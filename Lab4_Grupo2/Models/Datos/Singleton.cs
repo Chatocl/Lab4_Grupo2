46d5a05aec93a72ae49c88eb904adb56ccdc9c87
ROBERTO ALFREDO MOYA NOACK <ramoyan@correo.url.edu.gt>
2022-03-23 17:49:02 -0600

    Singleton

---
+++ Lab4_Grupo2/Models/Datos/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab4_Grupo2.Models;
+
+namespace Lab4_Grupo2.Models.Datos
+{
+    public class Singleton
+    {
+
+        private static Singleton _instance = null;
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Singleton();
+                return _instance;
+            }
+        }
+
+    }
+}
