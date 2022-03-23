ccefab9cf770453151971e36f08df1d4f0f53745
CARLOS ROBERTO CORONADO LAZO <crcoronado@correo.url.edu.gt>
2022-03-23 17:09:57 -0600

    Creacion de la Clase Paciente

---
+++ Lab4_Grupo2/Models/Paciente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab4_Grupo2.Models
+{
+    public class Paciente
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Apellidos { get; set; }
+
+        [Display(Name = "Fecha de nacimiento")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime? FDNacimiento { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Sexo { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Especializacion { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string MIngreso { get; set; }
+
+    }
+}
