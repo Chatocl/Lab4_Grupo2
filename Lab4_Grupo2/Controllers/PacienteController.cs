cf1534e979ce995769769738a82f21019ddb2dd5
ROBERTO ALFREDO MOYA NOACK <ramoyan@correo.url.edu.gt>
2022-03-30 13:18:37 -0600

    Links botones corregidos

--- Lab4_Grupo2/Controllers/PacienteController.cs
+++ Lab4_Grupo2/Controllers/PacienteController.cs
@@ -50,7 +50,7 @@ namespace Lab4_Grupo2.Controllers
                 prioridad = newPaciente.Delegado(newPaciente.Sexo,edad,newPaciente.Especializacion,newPaciente.MIngreso);
                 Singleton.Instance.Pacientes.Add(newPaciente, DateTime.Now, prioridad);
 
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(SalaEspera));
             }
             catch
             {
